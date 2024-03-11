a06eaf74f8aab29892603c54e36ed1d01f7976d0
oboy21 <oboy11b@gmail.com>
2024-03-11 07:37:55 +0300

    Code experiments

--- gui.py
+++ gui.py
@@ -8,6 +8,9 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+button_labels = ["Close", "Apply"]
+
+
 
 window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
