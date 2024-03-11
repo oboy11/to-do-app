9ea92ef3cb1d22fe3784b0bea1b2c04d171ac5bb
oboy21 <oboy11b@gmail.com>
2024-03-11 19:10:21 +0300

    Complete button

--- gui.py
+++ gui.py
@@ -8,12 +8,16 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-button_labels = ["Close", "Apply"]
+complete_button = sg.Button("Complete")
 
 
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                   font=('Helvetica', 20))
+
+window = sg.Window('My To-Do App', layout=[[label],
+                                           [input_box, add_button],
+                                           [list_box, edit_button,
+                                                 complete_button]],
+                                        font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
@@ -35,6 +39,14 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
